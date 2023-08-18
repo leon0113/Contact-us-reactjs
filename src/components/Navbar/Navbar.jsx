1c4261f860424d34fa3c5660c5911060341b0769
Leon <tahjib.leon399@gmail.com>
2023-08-18 16:47:09 +0600

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <img src="/images/logo.png" alt="Logo" />
+            </div>
+            <ul>
+                <li><a href="#"></a>Home</li>
+                <li><a href=""></a>About</li>
+                <li><a href=""></a>Contact</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
