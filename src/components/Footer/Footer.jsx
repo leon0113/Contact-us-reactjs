33005f8803db8312f0203f1d1e4a408427c4fab7
Leon <tahjib.leon399@gmail.com>
2023-09-01 23:34:48 +0600

    footer add

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h1>
+                copyright @ 2023
+            </h1>
+        </div>
+    );
+};
+
+export default Footer;
